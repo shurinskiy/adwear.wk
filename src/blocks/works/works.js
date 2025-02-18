095a338350edc32358fae917fb2f724efb162132
Alexandr Tumanov <shurinskiy2@gmail.com>
2025-02-19 01:00:15 +0500

    add works section

--- src/blocks/works/works.js
+++ src/blocks/works/works.js
@@ -1,4 +1,47 @@
-(() => {
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 
+(() => {
+	new Swiper(document.querySelector('.works__cards.swiper'), {
+		modules: [Navigation],
+		watchOverflow: true,
+		spaceBetween: 24,
+		threshold: 10,
+		navigation: {
+			nextEl: '.works__control_next',
+			prevEl: '.works__control_prev',
+			lockClass: 'hidden'
+		},
+		breakpoints: {
+			0: {
+				spaceBetween: 16,
+				slidesPerView: 1.15,
+			},
+			480: {
+				spaceBetween: 16,
+				slidesPerView: 1.8,
+			},
+			640: {
+				spaceBetween: 16,
+				slidesPerView: 2.4,
+			},
+			780: {
+				spaceBetween: 16,
+				slidesPerView: 2.7,
+			},
+			960: {
+				spaceBetween: 24,
+				slidesPerView: 3,
+			},
+			1100: {
+				spaceBetween: 24,
+				slidesPerView: 3.6,
+			},
+			1280: {
+				spaceBetween: 24,
+				slidesPerView: 4,
+			},
+		}
+	});
 
 })();
