662fa3438964fd6fb74e8d549330dbed1875aa96
Alexandr Tumanov <shurinskiy2@gmail.com>
2025-03-31 21:32:52 +0500

    add inputmask in modal

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -1,11 +1,18 @@
 import { disablePageScroll, enablePageScroll } from '@fluejs/noscroll';
 import { makeModal } from "../../js/libs/makeModal";
+import Inputmask from "inputmask";
 
 (() => {
 	makeModal({
 		open: function(modal, button) {
-			this.querySelector('.modal__bye')?.addEventListener('click', e => modal.close());
 			disablePageScroll();
+			
+			Inputmask({ 
+				"mask": "+7 (999) 999-99-99", 
+				showMaskOnHover: false 
+			}).mask(this.querySelectorAll('input[type="tel"]'));
+
+			this.querySelector('.modal__bye')?.addEventListener('click', e => modal.close());
 		},
 		close: function() {
 			enablePageScroll();
